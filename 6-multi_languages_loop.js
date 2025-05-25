8467a2a141a8ab1aeac21e0732089c0693471268
Valz-z <chidianya3@gmail.com>
2025-05-25 17:28:17 -0400

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,11 @@
-onst lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let output = "";
 
 for (let i = 0; i < lines.length; i++) {
-  console.log(lines);
+  output += lines[i];
+  if (i < lines.length - 1) {
+    output += "\n";
+  }
 }
+
+console.log(output);
